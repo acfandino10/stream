9627e395cd078ebfaf8f0b49620d2e9dee485c55
Ana <ana.fandino@inalambria.com>
2020-10-27 22:46:44 -0500

    udp connection python

---
+++ receiver.py
@@ -0,0 +1,36 @@
+import socket
+import struct
+import cv2
+import numpy as np
+
+MAX_DGRAM = 2**16
+def dump_buffer(s):
+  while True:
+    seg, addr = s.recvfrom(MAX_DGRAM)
+    print(seg[0])
+    if struct.unpack('B', seg[0:1])[0] == 1:
+      print('finish emptying buffer')
+      break
+def main():
+ 
+ # Set up socket
+ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ s.bind(('127.0.0.1', 12345))
+ dat = b''
+ dump_buffer(s)
+ while True:
+   seg, addr = s.recvfrom(MAX_DGRAM)
+   if struct.unpack('B', seg[0:1])[0] > 1:
+     dat += seg[1:]
+   else:
+     dat += seg[1:]
+     img = cv2.imdecode(np.fromstring(dat, dtype=np.uint8), 1)
+     cv2.imshow('frame', img)
+     if cv2.waitKey(1) & 0xFF == ord('q'):
+       break
+     dat = b''
+ # cap.release()
+ cv2.destroyAllWindows()
+ s.close()
+
+main()
---
+++ sender.py
@@ -0,0 +1,49 @@
+import cv2
+import socket
+import math
+import struct
+import sys
+
+print(sys.argv)
+class FrameSegment(object):
+  MAX_DGRAM = 2**16
+  MAX_IMAGE_DGRAM = MAX_DGRAM - 64 # minus 64 bytes in case UDP frame overflown
+  print(MAX_IMAGE_DGRAM)
+  def __init__(self, sock, port, addr='127.0.0.1'):
+    self.s = sock
+    self.port = port
+    self.addr = addr
+  def udp_frame(self, img):
+    MAX_DGRAM = 2**16
+    MAX_IMAGE_DGRAM = MAX_DGRAM - 64
+    compress_img = cv2.imencode('.jpg', img)[1]
+    dat = compress_img.tobytes()
+    size = len(dat)
+    num_of_segments = math.ceil(size/(MAX_IMAGE_DGRAM))
+    array_pos_start = 0
+    
+    while num_of_segments:
+      array_pos_end = min(size, array_pos_start + MAX_IMAGE_DGRAM)
+      self.s.sendto(
+                   struct.pack('B', num_of_segments) +
+                   dat[array_pos_start:array_pos_end], 
+                   (self.addr, self.port)
+                   )
+      array_pos_start = array_pos_end
+      num_of_segments -= 1
+
+def main():
+  # Set UDP socket
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  port = 12345
+  fs = FrameSegment(s, port)
+ 
+  cap = cv2.VideoCapture(0)
+  while (cap.isOpened()):
+    _,frame = cap.read()
+    fs.udp_frame(frame)
+  cap.release()
+  cv2.destroyAllWindows()
+  s.close()
+
+main()
