19fb4cf6b8208f31fdf9336c19360dec0d9c64c8
Ana <ana.fandino@inalambria.com>
2020-10-28 00:26:52 -0500

    files

--- receiver.py
+++ receiver.py
@@ -7,7 +7,6 @@ MAX_DGRAM = 2**16
 def dump_buffer(s):
   while True:
     seg, addr = s.recvfrom(MAX_DGRAM)
-    print(seg[0])
     if struct.unpack('B', seg[0:1])[0] == 1:
       print('finish emptying buffer')
       break
@@ -24,7 +23,7 @@ def main():
      dat += seg[1:]
    else:
      dat += seg[1:]
-     img = cv2.imdecode(np.fromstring(dat, dtype=np.uint8), 1)
+     img = cv2.imdecode(np.frombuffer(dat, dtype=np.uint8), 1)
      cv2.imshow('frame', img)
      if cv2.waitKey(1) & 0xFF == ord('q'):
        break
--- sender.py
+++ sender.py
@@ -4,11 +4,10 @@ import math
 import struct
 import sys
 
-print(sys.argv)
+NUM_ARCHIVO = sys.argv[1]
 class FrameSegment(object):
   MAX_DGRAM = 2**16
   MAX_IMAGE_DGRAM = MAX_DGRAM - 64 # minus 64 bytes in case UDP frame overflown
-  print(MAX_IMAGE_DGRAM)
   def __init__(self, sock, port, addr='127.0.0.1'):
     self.s = sock
     self.port = port
@@ -37,8 +36,20 @@ def main():
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   port = 12345
   fs = FrameSegment(s, port)
- 
-  cap = cv2.VideoCapture(0)
+  
+  if NUM_ARCHIVO == '1':
+    print('entra')
+    cap = cv2.VideoCapture('./video.mp4')
+  elif NUM_ARCHIVO == '2':
+    print('entra2')
+    cap = cv2.VideoCapture('./woman.mp4')
+  elif NUM_ARCHIVO == '3':
+    print('entra2')
+    cap = cv2.VideoCapture('./pexels.mp4')
+  elif NUM_ARCHIVO == '0':
+    print('entra2')
+    cap = cv2.VideoCapture(0)
+
   while (cap.isOpened()):
     _,frame = cap.read()
     fs.udp_frame(frame)
